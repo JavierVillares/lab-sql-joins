20ea2533be0f85e72b5394b748bf044d67b49f33
Javivi <161762434+JavierVillares@users.noreply.github.com>
2024-05-03 13:45:07 +0200

    hello there

--- lab-sql-joins.sql
+++ lab-sql-joins.sql
@@ -18,3 +18,57 @@ FROM payment p
 JOIN rental r ON p.rental_id = r.rental_id
 JOIN staff s ON p.staff_id = s.staff_id
 GROUP BY s.store_id;
+
+-- 4. 
+SELECT c.name AS category_name, AVG(f.length) AS avg_running_time
+FROM film_category fc
+JOIN film f ON fc.film_id = f.film_id
+JOIN category c ON fc.category_id = c.category_id
+GROUP BY c.name;
+
+-- 5.
+SELECT c.name AS category_name, AVG(f.length) AS avg_running_time
+FROM film_category fc
+JOIN film f ON fc.film_id = f.film_id
+JOIN category c ON fc.category_id = c.category_id
+GROUP BY c.name
+ORDER BY avg_running_time DESC;
+
+-- 6. 
+SELECT f.title AS film_title, COUNT(*) AS rental_count
+FROM rental r
+JOIN inventory i ON r.inventory_id = i.inventory_id
+JOIN film f ON i.film_id = f.film_id
+GROUP BY f.film_id
+ORDER BY rental_count DESC
+LIMIT 10;
+
+-- 7. 
+SELECT 
+    CASE 
+        WHEN COUNT(*) > 0 THEN 'Available'
+        ELSE 'Not Available'
+    END AS availability
+FROM 
+    film f
+JOIN 
+    inventory i ON f.film_id = i.film_id
+JOIN 
+    store s ON i.store_id = s.store_id
+WHERE 
+    f.title = 'Academy Dinosaur'
+    AND s.store_id = 1;
+
+-- 8.
+SELECT 
+    f.title AS film_title,
+    IFNULL(CASE 
+        WHEN COUNT(i.inventory_id) > 0 THEN 'Available'
+        ELSE 'NOT available'
+    END, 'NOT available') AS availability_status
+FROM 
+    film f
+LEFT JOIN 
+    inventory i ON f.film_id = i.film_id
+GROUP BY 
+    f.film_id;
